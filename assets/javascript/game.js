2809ec67ab3ee005a233233416493fd640584855
catkins <catkinsjr28@gmail.com>
2017-06-10 18:35:15 -0400

    Final Layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,42 @@
-var globalCount = 0;
-//selects random number between 19-120 for user to guess
-function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-//sets a variable to call getRandomInt function
-var randNumber = getRandomInt(19, 120);
-//Log in Chrome console to verify the getRandomInt function works
-console.log(randNumber);
-//place value of the variable "randNumber" in the div with a span within it that has an id="randomNumber"
-$("#randomNumber").append(randNumber);
-//When the red gem is clicked, select a random number between 1-12.
-var redGemCounter = getRandomInt(1, 12);
-//log in Chrome console to verify the value of the redGemCounter variable works.
-console.log(redGemCounter);
-//When the purple gem is clicked, select a random number between 1-12.
-var purpleGemCounter = getRandomInt(1, 12);
-//log in Chrome console to verify the value of the purpleGemCounter variable works.
-console.log(purpleGemCounter);
-//When the green gem is clicked, select a random number between 1-12.
-var greenGemCounter = getRandomInt(1, 12);
-//log in Chrome console to verify the value of the greenGemCounter variable works.
-console.log(greenGemCounter);
-//When the clear gem is clicked, select a random number between 1-12.
-var clearGemCounter = getRandomInt(1, 12);
-//log in Chrome console to verify the value of the clearGemCounter variable works.
-console.log(clearGemCounter);
-//When the red gem is clicked, add 1 to the current count
-$("#redGem").click(function() {
-	alert("YES!!!");
+$(document).ready(function() {
+	var globalCount = 0;
+	//selects random number between 19-120 for user to guess
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	//sets a variable to call getRandomInt function
+	var randNumber = getRandomInt(19, 120);
+	//Log in Chrome console to verify the getRandomInt function works
+	console.log(randNumber);
+	//place value of the variable "randNumber" in the div with a span within it that has an id="randomNumber"
+	$("#randomNumber").append(randNumber);
+	//update the value of variable "randNumber" so that it is centered and has a font size of 100px
+	$("#randomNumber").css({"font-size":"100px","text-align":"center"});
+	//When the red gem is clicked, select a random number between 1-12.
+	var redGemCounter = getRandomInt(1, 12);
+	//log in Chrome console to verify the value of the redGemCounter variable works.
+	console.log(redGemCounter);
+	//When the purple gem is clicked, select a random number between 1-12.
+	var purpleGemCounter = getRandomInt(1, 12);
+	//log in Chrome console to verify the value of the purpleGemCounter variable works.
+	console.log(purpleGemCounter);
+	//When the green gem is clicked, select a random number between 1-12.
+	var greenGemCounter = getRandomInt(1, 12);
+	//log in Chrome console to verify the value of the greenGemCounter variable works.
+	console.log(greenGemCounter);
+	//When the clear gem is clicked, select a random number between 1-12.
+	var clearGemCounter = getRandomInt(1, 12);
+	//log in Chrome console to verify the value of the clearGemCounter variable works.
+	console.log(clearGemCounter);
+
+	
+	//When the red gem is clicked, add 1 to the current count
+			$("#redGem").click(function() {
+			incrementRedGem();
+		alert("You clicked the Red Gem!");
+		
+	});
+	//show red gem count after each click
+		console.log(redGemCounter);
+	
 });
-//show red gem count afte each click
-console.log(redGemCounter);
